b321f54ca280c40eae2edb8f5fc68399bfc35709
chilam <chilam.Lee@outlook.com>
2024-11-19 18:01:31 +0800

    :bug: fix: fix 404

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   editPost: {
-    url: "https://github.com/ch1lam/ch1lam.github.io/edit/main/src/content/blog",
+    url: "https://github.com/ch1lam/ch1lam.github.io/edit/master/src/content/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
